7ead3525444285a044676930e716a05b3b2d854a
SolwDawg <113430055+SolwDawg@users.noreply.github.com>
2023-10-06 14:16:02 +0700

    submit c

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,5 @@ Route::get('admin', [AuthController::class, 'index'])->name('auth.admin');
 Route::post('login', [AuthController::class, 'login'])->name('auth.login');
 Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
 Route::get('dashboard/index', [DashboardController::class, 'index'])->name('dashboard.index')->middleware
-(AuthenticateMiddleware::class);
+('login');
 
